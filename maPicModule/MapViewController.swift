4c2b4c7b9ecf3f377d5056bb385375135ac445a9
Pedro Luis Berbel dos Santos <pedroberbel.br@gmail.com>
2017-04-07 15:22:54 -0300

    Added mapViewController, setted current user location, added all the other viewsCotrollers to this module

--- maPicModule/MapViewController.swift
+++ maPicModule/MapViewController.swift
@@ -7,29 +7,58 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var takePictureButton: UIButton!
+    
+    let manager = CLLocationManager()
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+     //Map and Location
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    
+        
+     // Picture
+        takePictureButton.layer.cornerRadius = 5
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+//        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+//        let region: MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        let region: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(myLocation, 500, 500)
+        mapView.setRegion(region, animated: false)
+        self.mapView.showsUserLocation = true
     }
-    */
+    
+    
+    
+    
+    @IBAction func takePictureButton(_ sender: Any) {
+        //will create a picLocal in the same localization of the user.
+    }
+    
+
 
 }
