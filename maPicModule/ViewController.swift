18de052ff241c93e65c42ad709945c0a8cfbc516
Pedro Luis Berbel dos Santos <pedroberbel.br@gmail.com>
2017-04-07 11:29:13 -0300

    Creating the project

---
+++ maPicModule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  maPicModule
+//
+//  Created by Pedro Luis Berbel dos Santos on 07/04/17.
+//  Copyright © 2017 Santosplb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
